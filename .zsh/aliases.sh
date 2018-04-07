2ebc7edc6836d0fbb5cfd034e6112afc4b639e1b
Tim <tapetri@wpi.edu>
2018-04-07 09:14:50 -0400

    Add zshrc and custom aliases script

---
+++ .zsh/aliases.sh
@@ -0,0 +1,7 @@
+# Access dotfiles repo
+alias home="git --work-tree=$HOME --git-dir=$HOME/.dotfiles/"
+
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
