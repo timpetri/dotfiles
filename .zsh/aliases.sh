3ccb964abcda53074e5079192efe5d8c46d10b3c
Tim Petri <tim.a.petri@gmail.com>
2018-11-12 23:49:56 -0500

    Add tmux files and git log alias

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -9,3 +9,5 @@ alias home="git --work-tree=$HOME --git-dir=$HOME/.dotfiles/"
 # Show/hide hidden files in Finder
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+alias gl="git log --graph --pretty=format:\"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset\" --abbrev-commit"
